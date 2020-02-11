7bc5af7cb422d4ca8f4a4f3cb271e7058d0547c3
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2020-02-11 23:39:20 +0900

    1hour 2min 00sec

---
+++ 등교길.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <memory.h>
+#include <iostream>
+using namespace std;
+//1:02:00
+int solution(int m, int n, vector<vector<int>> puddles) {
+    int MAP[101][101] = {0,};
+    int DP[101][101];
+    for(int i=0;i<puddles.size();i++){
+        MAP[puddles[i][1]][puddles[i][0]] = -1;}
+    DP[0][1]=1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(MAP[i][j]==-1)
+                DP[i][j]=0;
+            else
+                DP[i][j] = (DP[i-1][j]+DP[i][j-1])%1000000007;
+        }
+    }
+    return DP[n][m];
+}
