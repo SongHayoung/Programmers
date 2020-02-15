1e7b8178013f322544912cbd1f931b1742382eee
SongHayoung <lovelydays95@naver.com>
2020-02-15 19:41:29 +0900

    30min 0sec 문제오류있음

--- 징검다리.cpp
+++ 징검다리.cpp
@@ -1,43 +1,31 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+int SIZE;
 //정답이 아니나 정답으로 인정되는 코드
 /*
  inline bool is_possible(vector<int>& rocks, int &stnd, int &n, int &dis){
- int removed = 0, prev = 0, i;
- for(i=0;i<rocks.size();i++){
- if(rocks[i]-prev<stnd)
- ++removed;
- else
- prev = rocks[i];
- }
- if((dis-rocks[i])<dis)
- return (removed+1)<=n;
- return removed<=n;
+ register int removed = 0, prev = 0, i;
+ for(i=0;i<SIZE;i++){
+ rocks[i]-prev<stnd ? ++removed : prev = rocks[i];
+ 
+ return (dis-rocks[i])<dis ? (removed+1)<=n : removed<=n;
  }*/
 //정답코드
 inline bool is_possible(vector<int>& rocks, int &stnd, int &n, int &dis){
-    int removed = 0, prev = 0, i;
-    for(i=0;i<rocks.size();i++){
-        if(rocks[i]-prev<stnd){
-            ++removed;
-        }
-        else
-            prev = rocks[i];
-    }
-    if((dis-rocks[i-1])<stnd)
-        return ++removed<=n;
-    return removed<=n;
+    register int removed = 0, prev = 0, i;
+    for(i=0;i<SIZE;i++)
+        rocks[i]-prev<stnd ? ++removed : prev = rocks[i];
+    
+    return dis-rocks[i-1]<stnd ? ++removed<=n : removed<=n;
 }
 int solution(int distance, vector<int> rocks, int n) {
     sort(rocks.begin(),rocks.end());
-    int lo = 1, hi = distance, mid;
+    SIZE = rocks.size();
+    register int lo = 1, hi = distance, mid;
     while(lo+1<hi){
-        mid = (lo+hi)/2;
-        if(is_possible(rocks,mid,n,distance))
-            lo = mid;
-        else
-            hi = mid;
+        mid = (lo+hi)>>1;
+        is_possible(rocks,mid,n,distance) ? lo = mid : hi = mid ;
     }
-    return is_possible(rocks,lo,n,distance) ?lo : hi;
+    return is_possible(rocks,lo,n,distance) ? lo : hi;
 }
