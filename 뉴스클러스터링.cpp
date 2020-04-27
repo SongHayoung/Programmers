23779148351f311771509dd5674803cb102602e3
SongHayoung <37806785+SongHayoung@users.noreply.github.com>
2020-04-27 11:44:14 +0900

    13min 2sec

--- 뉴스클러스터링.cpp
+++ 뉴스클러스터링.cpp
@@ -1,7 +1,6 @@
 #include <string>
 #include <set>
 #include <algorithm>
-#include <iostream>
 using namespace std;
 bool check_validation(char &c1, char &c2, string &output){
     if((('a'<=c1&&c1<='z')||('A'<=c1&&c1<='Z'))&&(('a'<=c2&&c2<='z')||('A'<=c2&&c2<='Z'))){
@@ -11,18 +10,17 @@ bool check_validation(char &c1, char &c2, string &output){
     }
     return false;
 }
+void makeMultiset(string &s, multiset<string> &ms){
+    for(int i = 0; i < s.length()-1;++i){
+        string push;
+        if(check_validation(s[i],s[i+1],push))
+            ms.emplace(push);
+    }
+}
 int solution(string str1, string str2) {
     multiset<string> s1, s2, unions, intersections;
-    for(int i = 0; i < str1.length()-1;++i){
-        string push;
-        if(check_validation(str1[i],str1[i+1],push))
-            s1.emplace(push);
-    }
-     for(int i = 0; i < str2.length()-1;++i){
-        string push;
-        if(check_validation(str2[i],str2[i+1],push))
-            s2.emplace(push);
-    }
+    makeMultiset(str1,s1);
+    makeMultiset(str2,s2);
     set_union(s1.begin(), s1.end(), s2.begin(), s2.end(), inserter(unions, unions.begin()));
     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(), inserter(intersections, intersections.begin()));
     return unions.empty()? 65536 : 65536*intersections.size()/unions.size();
